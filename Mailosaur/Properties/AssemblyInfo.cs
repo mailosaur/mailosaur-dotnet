6941ad80f1f0dd355755f6e128524580cef38185
Andy Dobbels <andy@mailosaur.com>
2016-04-26 02:03:10 +0100

    fix Newtonsoft.JSON version, upgrade and publish 3.0.1 of Mailosaur dot net client library to nugget

--- Mailosaur/Properties/AssemblyInfo.cs
+++ Mailosaur/Properties/AssemblyInfo.cs
@@ -2,11 +2,11 @@ using System.Reflection;
 using System.Runtime.CompilerServices;
 
 [assembly: AssemblyTitle("Mailosaur")]
-[assembly: AssemblyDescription(".NET client for Mailosaur.com")]
+[assembly: AssemblyDescription(".NET client library for Mailosaur.com")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("Clickity Ltd")]
 [assembly: AssemblyProduct("mailosaur.com")]
 [assembly: AssemblyCopyright("Clickity Ltd 2016")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
-[assembly: AssemblyVersion("3.0.0")]
+[assembly: AssemblyVersion("3.0.1")]
