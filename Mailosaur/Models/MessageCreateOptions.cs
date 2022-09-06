e920ac9354ca61e46a559f97848aec9274cb2f9d
Jon M <jon@mailosaur.com>
2022-09-06 12:54:35 +0100

    Add 'from' support to MessageCreateOptions

--- Mailosaur/Models/MessageCreateOptions.cs
+++ Mailosaur/Models/MessageCreateOptions.cs
@@ -15,6 +15,8 @@ namespace Mailosaur.Models
         /// Initializes a new instance of the MessageCreateOptions class.
         /// </summary>
         /// <param name="to">The email address to which the email will be sent.</param>
+        /// <param name="from">Partially overrides of the message's 'from' address. This **must** be an address ending 
+        /// with `YOUR_SERVER.mailosaur.net`, such as `my-emails @a1bcdef2.mailosaur.net`.</param>
         /// <param name="send">If true, email will be sent upon creation.</param>
         /// <param name="subject">The email subject line.</param>
         /// <param name="text">The plain text body of the email. Note that only text or html can be supplied, not both.</param>
@@ -34,6 +36,12 @@ namespace Mailosaur.Models
         /// The email address to which the email will be sent.
         /// </summary>
         public string To { get; set; }
+
+        /// <summary>
+        /// Partially overrides of the message's 'from' address. This **must** be an address ending 
+        /// with `YOUR_SERVER.mailosaur.net`, such as `my-emails @a1bcdef2.mailosaur.net`.
+        /// </summary>
+        public string From { get; set; }
 
         /// <summary>
         /// If true, email will be sent upon creation.
