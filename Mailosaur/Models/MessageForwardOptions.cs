77b8362cf671cacaad96dd21ff68078414121096
ey-mailosaur <ethan.y@mailosaur.com>
2024-12-19 11:39:15 +0000

    Added CC property to message option models
    Including Create, Forward and Reply

--- Mailosaur/Models/MessageForwardOptions.cs
+++ Mailosaur/Models/MessageForwardOptions.cs
@@ -13,9 +13,10 @@ namespace Mailosaur.Models
         /// Initializes a new instance of the MessageForwardOptions class.
         /// </summary>
         /// <param name="to">The email address to which the email will be sent.</param>
+        /// <param name="cc">The email address to which the email will be CC'd.</param>
         /// <param name="text">Any additional plain text content to forward the email with. Note that only text or html can be supplied, not both.</param>
         /// <param name="html">Any additional HTML content to forward the email with. Note that only html or text can be supplied, not both.</param>
-        public MessageForwardOptions(string to, string text = null, string html = null)
+        public MessageForwardOptions(string to, string text = null, string html = null, string cc = null)
         {
             To = to;
             Text = text;
@@ -26,6 +27,11 @@ namespace Mailosaur.Models
         /// The email address to which the email will be sent.
         /// </summary>
         public string To { get; set; }
+        
+        /// <summary>
+        /// The email address to which the email will be CC'd.
+        /// </summary>
+        public string Cc { get; set; }
         
         /// <summary>
         /// Any additional plain text content to forward the email with. Note that only text or html can be supplied, not both.
